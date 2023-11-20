332b34f7ce4995f3d4b49415675273502338bade
ron-elmekiesse <ronelmekiesse@gmail.com>
2023-11-20 23:07:06 +0200

    Added SendEth usage

--- Dev/main.go
+++ Dev/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"fmt"
+
 	"PlayWithCrypto.com/RETH"
 )
 
+const (
+	URL_TO_CONNECT          string = "https://sepolia.infura.io/<ENTER_API_KEY>"
+	SENDER_PRIVATE_KEY      string = "<ENTER_SENDER_PRIVATE_KEY>"
+	WEI_TO_SEND             int64  = 0
+	RECEIVER_PUBLIC_ADDRESS string = "<ENTER_RECEIVER_PUBLIC_ADDRESS>"
+)
+
 func main() {
-	RETH.Hello()
+	fmt.Println(URL_TO_CONNECT)
+	fmt.Println(SENDER_PRIVATE_KEY, "(", WEI_TO_SEND, ")")
+	fmt.Println("\t|")
+	fmt.Println("\tV")
+	fmt.Println(RECEIVER_PUBLIC_ADDRESS)
+
+	RETH.SendEth(URL_TO_CONNECT, SENDER_PRIVATE_KEY, WEI_TO_SEND, RECEIVER_PUBLIC_ADDRESS)
 }
